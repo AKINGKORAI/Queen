8873a99fcbe6cc4bd1c29c4b5fb1f00215177b64
AKINGKORAI <118323297+AKINGKORAI@users.noreply.github.com>
2022-12-25 08:18:45 +0500

    Update Queen.py

--- Queen.py
+++ Queen.py
@@ -1,1 +1,17 @@
-import Queen
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Quuen import Legend
+    Legend()
+elif bit == '32bit':
+    from Queen32 import Legend
+    Legend()
+else:
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
+    os.system('exit')
+ 
